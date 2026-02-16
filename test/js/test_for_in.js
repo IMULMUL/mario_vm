32c404620d85e4a6a7cc379486921ec0a28cd931
misazhu <misa.zhu@gmail.com>
2026-02-17 01:15:59 +0800

    for-in implemented

---
+++ test/js/test_for_in.js
@@ -0,0 +1,13 @@
+var obj = new Date();
+for (var key in obj) {
+    console.log("   " + key + ": " + obj[key]);
+}
+
+console.log("\n");
+
+
+var obj1 = {b: 1};
+for (var key in obj1) {
+    console.log("   " + key + ": " + obj1[key]);
+}
+
