5d7c4eda683c0b6c0ff9c1170c4e59560f4650cb
misazhu <misa.zhu@gmail.com>
2026-02-20 03:40:19 +0800

    load node cache

--- test/js/bench.js
+++ test/js/bench.js
@@ -1,14 +1,28 @@
+console.log("benchmark while loop");
+console.log("-------------------------");
+var start_while = Date.now();
 var a = 0;
 while(a <= 10000000) {
-	if((a % 100000) == 0)
+	if((a % 1000000) == 0) {
 		console.log(a);
+	}
 	a++;
 }
+var end_while = Date.now();
 
+console.log("\nbenchmark for loop");
+console.log("-------------------------");
+var start_for = Date.now();
 var b = {i: 0};
 for(var i=0; i<=10000000; i++) {
-	if((i % 100000) == 0)
+	if((i % 1000000) == 0) {
 		console.log(b.i);
+	}
 	b.i++;
 }
+var end_for = Date.now();
+
+console.log("\n-------------------------");
+console.log("while loop time: " + (end_while - start_while) + "ms");
+console.log("for loop time  : " + (end_for - start_for) + "ms");
 
