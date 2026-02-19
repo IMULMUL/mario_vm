e61bd5cf10e3973bcdd18f8bc79d1486ac1e9bcb
misazhu <misa.zhu@gmail.com>
2026-02-20 00:34:43 +0800

    remove ic_cache

--- test/js/bench.js
+++ test/js/bench.js
@@ -1,7 +1,14 @@
-a = 0;
-
-while(a < 10000000) {
-	a++;
+var a = 0;
+while(a <= 10000000) {
 	if((a % 100000) == 0)
 		console.log(a);
+	a++;
 }
+
+var b = {i: 0};
+for(var i=0; i<=10000000; i++) {
+	if((i % 100000) == 0)
+		console.log(b.i);
+	b.i++;
+}
+
